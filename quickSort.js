2cb0b78fccf58352e0e0527c60ad0e7d78252856
eduardo <edu_felip@hotmail.com>
2020-06-08 18:30:50 -0300

    finished

--- quickSort.js
+++ quickSort.js
@@ -1,7 +1,7 @@
 const quickSort = function(list, low, high) {
   let init = low
   let finish = init
-  const pivotIndex = high 
+  const pivotIndex = high
   const pivot = list[pivotIndex]
   while(finish <= pivotIndex - 1) {
     if(list[finish] < pivot) {
@@ -15,42 +15,34 @@ const quickSort = function(list, low, high) {
   if(init >= low) {
     list.splice(init, 0, list[pivotIndex])
     list.splice(pivotIndex + 1, 1)
-    if(init > low + 1) quickSort(list, low, init - 1)
+    if(init - low > 1) quickSort(list, low, init - 1)
+    if(high - init > 1) quickSort(list, init + 1, high)
   }
   return list
 }
 
-// const part = function(list, low, high) {
-//   if(low < high){
+const list = [
+  [1, 3, 2, 5, 4],
+  [9, 2, 8, 3, 7],
+  [1, 3, 2, 5, 4, 9, 8, 12, 0],
+  [9, 0, 9, 0]
+]
+const check = [
+  [1, 2, 3, 4, 5],
+  [2, 3, 7, 8, 9],
+  [0, 1, 2, 3, 4, 5, 8, 9, 12],
+  [0, 0, 9, 9]
+]
+const results = []
 
-//   }
-// }
-const sorted = quickSort([4, 7, 2, 6, 4, 1, 8, 3], 0, 7)
-console.log(sorted)
+list.forEach(arr => {
+  results.push(quickSort(arr, 0, arr.length - 1))
+})
 
-
-// const listOne = [1, 3, 2, 5, 4]
-// const listTwo = [9, 2, 8, 3, 7]
-// const listThree = [1, 3, 2, 5, 4, 9, 8, 12, 0]
-
-// const resultOne = [1, 2, 3, 4, 5]
-// const resultTwo = [2, 3, 7, 8, 9]
-// const resultThree = [0, 1, 2, 3, 4, 5, 8, 9, 12]
-
-// if(quickSort(listOne) != resultOne) {
-//   console.log("resultado 1 correto")
-// } else {
-//   console.log("resultado 1 errado")
-// }
-
-// if(quickSort(listTwo) != resultTwo) {
-//   console.log("resultado 2 correto")
-// } else {
-//   console.log("resultado 2 errado")
-// }
-
-// if(quickSort(listThree) != resultThree) {
-//   console.log("resultado 3 correto")
-// } else {
-//   console.log("resultado 3 errado")
-// }
+for(let i = 0; i < list.length ; i++){
+  if(check[i].toString() === results[i].toString()) {
+    console.log(`array ${i+1} was sorted correctly`)
+  } else {
+    console.log(`array ${i+1} wasn't sorted correctly, try again`)
+  }
+}
