2f684748145844d0d4ad6811eeb65c00dc8f461b
eduardo <edu_felip@hotmail.com>
2020-06-09 17:08:40 -0300

    small corretions

--- quickSort.js
+++ quickSort.js
@@ -27,22 +27,20 @@ const list = [
   [1, 3, 2, 5, 4, 9, 8, 12, 0],
   [9, 0, 9, 0]
 ]
-const check = [
-  [1, 2, 3, 4, 5],
-  [2, 3, 7, 8, 9],
-  [0, 1, 2, 3, 4, 5, 8, 9, 12],
-  [0, 0, 9, 9]
-]
 const results = []
 
 list.forEach(arr => {
-  results.push(quickSort(arr, 0, arr.length - 1))
+  results.push(quickSort(arr, 0, arr.length))
 })
 
-for(let i = 0; i < list.length ; i++){
-  if(check[i].toString() === results[i].toString()) {
-    console.log(`array ${i+1} was sorted correctly`)
+for(let j = 0; j < list.length ; j++){
+  const jsSort = list[j].sort((a, b) => a - b)
+  const qSort = quickSort(list[j], 0, list[j].length)
+  if(jsSort === qSort) {
+    console.log(`array ${j+1} was sorted correctly`)
   } else {
-    console.log(`array ${i+1} wasn't sorted correctly, try again`)
+    console.log(`array ${j+1} wasn't sorted correctly, try again`)
+    console.log(jsSort)
+    console.log(qSort)
   }
 }
