38b9c37858f4971f1917d0c6504e44b016a6e0d3
eduardo <edu_felip@hotmail.com>
2020-10-13 17:33:48 -0300

    improved tests file

--- tests.c
+++ tests.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "selection.c"
 
 int main() {
-  int num[4] = {9, 0, 9, 0};
-  int length = sizeof(num)/sizeof(int);
+  int *num;
+  int length;
+  printf("Insert the length\n");
+  scanf("%d", &length);
+  num = (int *) malloc(length*sizeof(int));
+  if (num == NULL) { printf("Lacks memory\n"); exit(1); }
+  printf("Insert the numbers\n");
+  for(int i = 0; i < length; i++){
+    scanf("%d", &num[i]);
+  }
   selection(num, length);
+  printf("----------\n");
   for(int i = 0; i < length; i++){
     printf("%d\n", num[i]);
   }
+  free(num);
   return 0;
 }
